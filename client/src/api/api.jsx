ed64948f69953b583c4e1fd365e50917b740b631
DhruvPatel <dhruvsingh4820@gmail.com>
2026-08-13 23:56:17 -0700

    making some changes

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -15,10 +15,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:5000/api/v1",
-  baseURL: "https://devconnect-frontend-qipf.onrender.com/api/v1",
+  baseURL: "https://devconnect-backend-lgof.onrender.com/api/v1",
   withCredentials: true,
-
   headers: {
     "Content-Type": "application/json",
   },
