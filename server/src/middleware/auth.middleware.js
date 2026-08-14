9ac530395599f4b3a3fd4b17560cffaed13b7f48
DhruvPatel <dhruvsingh4820@gmail.com>
2026-08-14 00:08:39 -0700

    mising some part

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -4,6 +4,8 @@ const ApiError = require("../utils/ApiError");
 const asyncHandler = require("../utils/asyncHandler");
 
 exports.verifyJWT = asyncHandler(async (req, res, next) => {
+  console.log("Cookies received:", Object.keys(req.cookies || {}));
+  console.log("Has accessToken:", !!req.cookies?.accessToken);
   const token =
     req.cookies?.accessToken ||
     req.header("Authorization")?.replace("Bearer ", "");
@@ -12,13 +14,10 @@ exports.verifyJWT = asyncHandler(async (req, res, next) => {
     throw new ApiError(401, "Unauthorized request");
   }
 
-  const decodedToken = jwt.verify(
-    token,
-    process.env.ACCESS_TOKEN_SECRET
-  );
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
   const user = await User.findById(decodedToken._id).select(
-    "-password -refreshToken"
+    "-password -refreshToken",
   );
 
   if (!user) {
